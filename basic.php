7d91599abdf6a473f813f6c7993def7cb16ba789
JLi7733 <48297521+JLi7733@users.noreply.github.com>
2021-02-28 11:58:19 -0600

    Delete basic.php

--- basic.php
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-
-Thanks for submitting the form  <?php echo $_POST["fname"]; ?> <?php echo $_POST["lname"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
-
-</body>
-</html>
